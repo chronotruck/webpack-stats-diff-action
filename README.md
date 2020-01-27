ca678417131078bacdc017e46f0bfdf8021cc3ea
William DA SILVA <william.da.silva@outlook.com>
2020-01-27 15:10:24 +0100

    feat: add Action

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Webpack stats diff Github Action
+
+Creates a comment in your PR comparing two stats files.
+
+## Inputs
+
+### `base_stats_path`
+
+**Required** Path to the Webpack generated "stats.json" file from the base branch.
+
+### `head_stats_path`
+
+**Required** Path to the Webpack generated "stats.json" file from the head branch.
+
+### `token`
+
+**Required** Github token so the package can publish a comment in the pull-request when the diff is ready.
+
+## Usage
+
+```yml
+uses: chronotruck/webpack-stats-diff-action@v1
+with:
+  base_stats_path: '/path/to/my/stats.json'
+  head_stats_path: '/path/to/my/stats.json'
+  token: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ index.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const fs = require('fs')
+const github = require('@actions/github')
+const core = require('@actions/core')
+const { getStatsDiff } = require('webpack-stats-diff')
+const prettySize = require('prettysize')
+const markdownTable = require('markdown-table')
+
+const doesPathExists = path => {
+  if (!fs.existsSync(path)) {
+    throw new Error(`${path} does not exist!`)
+  }
+}
+
+async function run() {
+  try {
+    const statsPaths = {
+      base: core.getInput('base_stats_path'),
+      head: core.getInput('head_stats_path')
+    }
+
+    const paths = {
+      base: path.resolve(process.cwd(), statsPaths.base),
+      head: path.resolve(process.cwd(), statsPaths.head)
+    }
+
+    doesPathExists(paths.base)
+    doesPathExists(paths.head)
+
+    const assets = {
+      base: require(paths.base).assets,
+      head: require(paths.head).assets
+    }
+
+    const diff = getStatsDiff(assets.base, assets.head, {})
+
+    const summaryTable = markdownTable([
+      [
+        'Old size',
+        'New size',
+        'Diff'
+      ],
+      [
+        prettySize(diff.total.oldSize),
+        prettySize(diff.total.newSize), 
+        `${prettySize(diff.total.diff)} (${diff.total.diffPercentage.toFixed(2)}%)`
+      ]
+    ])
+    
+    /**
+     * Publish a comment in the PR with the diff result.
+     */
+    const octokit = new github.GitHub(core.getInput('token'))
+
+    const pullRequestId = github.context.issue.number
+    if (!pullRequestId) {
+      throw new Error('Cannot find the PR id.')
+    }
+
+    await octokit.issues.createComment({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: pullRequestId,
+      body: `# Bundle difference
+## Total summary
+${summaryTable}
+`
+    })
+  }
+  catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
